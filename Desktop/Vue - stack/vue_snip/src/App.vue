25c510822d90e0da361af274c99ef8a1b022adb7
SPS992 <hello@samsedgeman.co.uk>
2020-08-25 19:58:45 +0100

    Removing empty container to possibly solve Heroku build error

---
+++ Desktop/Vue - stack/vue_snip/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/create">Create</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+@import "assets/css/tailwind.css";
+
+h1{padding-top: 2.5rem}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #B794F4;
+    }
+  }
+}
+</style>
---
+++ Desktop/Vue - stack/vue_snip/src/views/Edit.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class=" bg-gray-800 w-full h-screen text-white">
+    <div class="container text-center mx-auto">
+   <h1 class="text-5xl">Edit Snippet</h1>
+   
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
